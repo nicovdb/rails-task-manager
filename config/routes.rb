958c6b0aad0eb338b0eb27b48470141a0c38965e
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-05-08 14:44:04 +0200

    ex1done sans css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to:'tasks#index', as: :tasks
+  get "task/:id", to: "tasks#show", as: :task
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create", as: :create_task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "task/:id", to: "tasks#update", as: :update_task
+  delete "task/:id", to: "tasks#destroy", as: :destroy_task
 end
